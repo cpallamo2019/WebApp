ae51cb15eabf832d9de4620e5027f4ee121e0cd8
cpallamo2019 <conchi.palla@gmail.com>
2020-12-18 18:23:30 +0100

    table creation and navigation bar

---
+++ mywebapp.py
@@ -0,0 +1,36 @@
+import os
+#from forms import  AddForm , DelForm
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db) #to connect the app with db
+
+#Create table
+
+class Puppy(db.Model):
+
+    __tablename__ = 'projects'
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.Text)
+
+    def __init__(self,name):
+        self.name = name
+
+    def __repr__(self):
+        return f"Project name: {self.name}"
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
